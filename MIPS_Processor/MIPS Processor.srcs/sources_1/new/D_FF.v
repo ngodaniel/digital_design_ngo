5183fd0a4dae39acea0c87d73993525f8384f4b3
Daniel Ngo <ngodaniel1997@gmail.com>
2021-03-03 23:50:53 -0800

    completed dff module

--- MIPS_Processor/MIPS Processor.srcs/sources_1/new/D_FF.v
+++ MIPS_Processor/MIPS Processor.srcs/sources_1/new/D_FF.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
-// Engineer: 
+// Engineer: Daniel Ngo
 // 
 // Create Date: 03/03/2021 11:44:41 PM
 // Design Name: 
 // Module Name: D_FF
-// Project Name: 
+// Project Name: MIPS Processor
 // Target Devices: 
 // Tool Versions: 
-// Description: 
+// Description: d flip-flop apart of the ALU component of the MIPS processor
 // 
 // Dependencies: 
 // 
@@ -27,4 +27,11 @@ module D_FF(
     input init_value,
     output q
     );
+    reg q;
+    
+    always @(posedge clk or negedge rst_n)
+        if (~rst_n)
+            q <= init_value;    //set to zero on reset
+        else
+            q <= d;             
 endmodule
