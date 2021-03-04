8941cd65b49cb5face9bf0e1af66922a7e2c11f9
Daniel Ngo <ngodaniel1997@gmail.com>
2021-03-03 23:28:24 -0800

    Initial Commit

---
+++ MIPS_Processor/MIPS Processor.srcs/sources_1/new/adder.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Daniel Ngo
+// 
+// Create Date: 03/03/2021 11:20:03 PM
+// Design Name: 
+// Module Name: adder
+// Project Name: MIPS Processor
+// Target Devices: 
+// Tool Versions: 
+// Description: full adder apart of the ALU component of the MIPS processor
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module adder(
+    input a,
+    input b,
+    input cin,
+    output cout,
+    output sum
+    );
+    
+  
+endmodule
