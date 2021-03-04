684048678dc9dc8964c85a00ad289b34ed694150
Daniel Ngo <ngodaniel1997@gmail.com>
2021-03-03 23:45:23 -0800

    created d_ff module

---
+++ MIPS_Processor/MIPS Processor.srcs/sources_1/new/D_FF.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/03/2021 11:44:41 PM
+// Design Name: 
+// Module Name: D_FF
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module D_FF(
+    input d,
+    input rst_n,
+    input clk,
+    input init_value,
+    output q
+    );
+endmodule
